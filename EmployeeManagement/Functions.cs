f246a2bf12a7f3c1301b12a0c43968fc9588aaae
ESLAM <i.jamal.ict@btu.edu.eg>
2022-12-14 22:49:26 +0200

    Create Function class

---
+++ EmployeeManagement/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    class Functions
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+
+        public Functions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Eslam\OneDrive\Documents\DbEmp.mdf;Integrated Security=True;Connect Timeout=30";
+            con = new SqlConnection(ConStr);
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+        }
+
+        
+}
+}
